853aec46e0d6721803b09859976225c1a0c2ea42
Charbel <charbelghosn24@hotmail.com>
2024-10-21 16:43:41 -0700

    deploy config

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const path = require ("path")
 const routes = require("./routes");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname,"../client/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../client/dist/index.html"))
+})
+
 
 app.use('/api',routes)
 module.exports = app
