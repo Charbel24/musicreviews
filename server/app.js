75e9c9b03a04a008aed2ca2b75c34a2e23ed7e32
Charbel <charbelghosn24@hotmail.com>
2024-10-21 17:27:34 -0700

    static routes deploy

--- server/app.js
+++ server/app.js
@@ -8,9 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname,"../client/dist")))
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../client/dist/index.html"))
-})
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.join(__dirname,"../client/dist/index.html"))
+// })
 
 
 app.use('/api',routes)
